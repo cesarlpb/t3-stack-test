4b846a67ff3017f26b7d7159debc168a8c709734
César <cesarlpb@gmail.com>
2023-05-12 19:35:00 +0200

    Add - Clerk and middleware

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,16 @@
+// Imports
 import { type AppType } from "next/app";
-
 import { api } from "~/utils/api";
-
+import { ClerkProvider } from "@clerk/nextjs";
+// Hoja de estilos global
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+  <ClerkProvider {...pageProps}>
+    <Component {...pageProps} />
+  </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
