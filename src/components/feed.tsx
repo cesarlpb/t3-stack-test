7e5bb9ee42636a57658cde75c8c1e9103ad8b6d9
César <cesarlpb@gmail.com>
2023-05-15 00:56:51 +0200

    Tweak - Feed para ajustar a pantallas < 400 px

--- src/components/feed.tsx
+++ src/components/feed.tsx
@@ -7,38 +7,44 @@ import Img from "next/image";
 import { useUser } from "@clerk/nextjs";
 
 export const ProfilePicture = ({
-    authorImgUrl,
-    width = 32,
-    height = 32,
-    colSpan = 4,
-  }: {
-    authorImgUrl: string;
-    width?: number;
-    height?: number;
-    colSpan?: number;
-  }) => {
-    const { user, isLoaded: userLoaded } = useUser();
-    // console.log("user:", user);
-    if (!user && !userLoaded) return null;
-    return (
-      <div className={`me-3 col-span-${colSpan}`}>
-        <Img
-          className="rounded-full"
-          src={authorImgUrl || "/avatar.png"}
-          alt={user?.username ? `@${user?.username || ""} profile picture` : "profile picture"}
-          width={width ?? 24}
-          height={height ?? 24}
-        />
-      </div>
-    );
-  };
+  authorImgUrl,
+  width = 32,
+  height = 32,
+  colSpan = 4,
+}: {
+  authorImgUrl: string;
+  width?: number;
+  height?: number;
+  colSpan?: number;
+}) => {
+  const { user, isLoaded: userLoaded } = useUser();
+  // console.log("user:", user);
+  if (!user && !userLoaded) return null;
+  return (
+    <div className={`me-3 col-span-${colSpan}`}>
+      <Img
+        className="rounded-full"
+        src={authorImgUrl || "/avatar.png"}
+        alt={
+          user?.username
+            ? `@${user?.username || ""} profile picture`
+            : "profile picture"
+        }
+        width={width ?? 24}
+        height={height ?? 24}
+      />
+    </div>
+  );
+};
 
 type PostWithUser = RouterOutputs["posts"]["getAll"][number];
 export const PostView = (props: PostWithUser) => {
   const { post, author } = props;
   return (
     <div
-      className="mx-auto my-1 flex w-10/12 flex-row items-center justify-center rounded-xl bg-cyan-300/30 p-4 text-2xl text-white hover:bg-white/20"
+      className="w-12/12 mx-auto my-1 flex flex-row items-center justify-center 
+      rounded-xl bg-cyan-300/30 p-4 text-2xl text-white 
+      hover:bg-white/20 md:w-10/12"
       key={post.id}
     >
       <ProfilePicture
@@ -46,11 +52,13 @@ export const PostView = (props: PostWithUser) => {
         width={48}
         height={48}
       />
-      <div className="flex flex-col grow border-0 ps-5">
+      <div className="flex grow flex-col border-0 ps-5">
         <div className="flex flex-row align-middle">
-          <div className="text-xs md:text-sm text-slate-200 font-thin">{author ? `@${author?.username || ""}` : ""}</div>
-          <div className="text-xs md:text-sm text-slate-200/50 mx-2">·</div>
-          <div className="ms-3 text-xs text-slate-400 md:text-sm align-baseline">
+          <div className="text-xs font-thin text-slate-200 md:text-sm">
+            {author ? `@${author?.username || ""}` : ""}
+          </div>
+          <div className="mx-2 text-xs text-slate-200/50 md:text-sm">·</div>
+          <div className="ms-3 align-baseline text-xs text-slate-400 md:text-sm">
             {formatDistanceToNow(new Date(post.createdAt), {
               addSuffix: true,
               locale: es,
@@ -64,25 +72,24 @@ export const PostView = (props: PostWithUser) => {
 };
 
 export const Feed = (props: { postsNumber?: number }) => {
-    const { data, isLoading: postsLoading } = api.posts.getAll.useQuery();
-    let slicedData = data || [];
+  const { data, isLoading: postsLoading } = api.posts.getAll.useQuery();
+  let slicedData = data || [];
 
-    // const previewData = postsNumber ? data?.slice(0, 3) : data;
-    if (props.postsNumber) {
-      slicedData = data?.slice(0, props.postsNumber) || [];
-    }
-  
-    if (postsLoading) {
-      return <LoadingPage />;
-    } else if (!data) {
-      return <div className="">Vaya... esos emojis no llegan🫥</div>;
-    } else {
-      return (
-        <>
-          {slicedData.map((postWithAuthor) => (
-            <PostView {...postWithAuthor} key={postWithAuthor.post.id} />
-          ))}
-        </>
-      );
-    }
-  };
+  if (props.postsNumber) {
+    slicedData = data?.slice(0, props.postsNumber) || [];
+  }
+
+  if (postsLoading) {
+    return <LoadingPage />;
+  } else if (!data) {
+    return <div className="">Vaya... esos emojis no llegan🫥</div>;
+  } else {
+    return (
+      <>
+        {slicedData.map((postWithAuthor) => (
+          <PostView {...postWithAuthor} key={postWithAuthor.post.id} />
+        ))}
+      </>
+    );
+  }
+};
