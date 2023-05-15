0c55c34b2442dba45025e2d8f764f792d7e952a6
César <cesarlpb@gmail.com>
2023-05-15 04:57:01 +0200

    Fix - Solved issue with <Head>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,13 +5,24 @@ import { ClerkProvider } from "@clerk/nextjs";
 // Hoja de estilos global
 import "~/styles/globals.css";
 import { Toaster } from "react-hot-toast";
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-  <ClerkProvider {...pageProps}>
-    <Toaster position="bottom-center"/>
-    <Component {...pageProps} />
-  </ClerkProvider>
+  <>
+    <ClerkProvider {...pageProps}>
+      <Head>
+        <title>My Emojer App 😐 - A simple emoji-friendly app</title>
+        <meta
+          name="description"
+          content="A simple emoji-friendly app with NextJS, TS, Prisma, Planetscale, Vercel, Axiom, Tailwind and some other stuff -- probably, maybe."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Toaster position="bottom-center"/>
+      <Component {...pageProps} />
+    </ClerkProvider>
+    </>
   );
 };
 
