d7e6aff636b6e8693c1ebefd32d96201b6dd76e0
César <cesarlpb@gmail.com>
2023-05-15 04:56:03 +0200

    CustomLink - removed icons as of now

--- src/components/customLink.tsx
+++ src/components/customLink.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { faHome, faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { faArrowLeft, faArrowRight, faHome } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export function CustomLink(props: { icon?: string, href: string; text: string }) {
@@ -9,19 +9,17 @@ export function CustomLink(props: { icon?: string, href: string; text: string })
   //   : props.icon == "arrow-right" ? "arrow-right" : ""
   
   return (
-    
     <>
-      
       <Link
-        className="rounded-full bg-[hsl(280,100%,70%)] px-5 py-3 
+        className="rounded-full bg-[hsl(280,100%,70%)] px-10 py-2
               text-center font-semibold text-black no-underline transition 
               flex flex-row items-center justify-center gap-x-2
-              hover:bg-cyan-200/70 hover:text-slate-200"
+              hover:bg-cyan-200/70 hover:text-slate-200 text-[1rem]"
         href={props.href}
       >
-        {props.icon && props.icon == "home" ? <FontAwesomeIcon icon={faHome} /> 
-        : props.icon == "arrow-left" ? <FontAwesomeIcon icon={faArrowLeft} /> 
-        : props.icon == "arrow-right" ? <FontAwesomeIcon icon={faArrowRight} /> : ""}
+        {/* {props.icon && props.icon == "home" ? <FontAwesomeIcon icon={faHome} className="text-[1rem]" /> 
+        : props.icon == "arrow-left" ? <FontAwesomeIcon icon={faArrowLeft} className="text-[1rem]" /> 
+        : props.icon == "arrow-right" ? <FontAwesomeIcon icon={faArrowRight} className="text-[1rem]" /> : ""} */}
 
         {props.text}
       </Link>
