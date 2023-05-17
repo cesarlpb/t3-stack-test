a4b426a3a1f4b8f0474d622ceb7dba9d1da236e0
César <cesarlpb@gmail.com>
2023-05-17 23:42:52 +0200

    Add SSGHelper

---
+++ src/server/helpers/ssgHelper.ts
@@ -0,0 +1,13 @@
+import { createServerSideHelpers } from "@trpc/react-query/server";
+import { appRouter } from "~/server/api/root";
+import { prisma } from "~/server/db";
+import superjson from "superjson";
+
+export const generateSsgHelper = () => {
+  const helpers = createServerSideHelpers({
+    router: appRouter,
+    ctx: { prisma, userId: null },
+    transformer: superjson, // optional - adds superjson serialization
+  });
+  return helpers;
+};
